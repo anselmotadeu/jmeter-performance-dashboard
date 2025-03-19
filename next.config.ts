f2189eca2a026ec609959d8f06da565030f19bb9
anselmotadeu <anselmotadeu@outlook.com>
2025-03-19 12:52:21 -0300

    Ignorar ESLint no build de produção no Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /* outras configs que você queira adicionar depois */
 };
 
 export default nextConfig;
